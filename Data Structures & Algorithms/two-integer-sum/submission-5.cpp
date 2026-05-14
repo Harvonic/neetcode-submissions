43a874f47af941cf52ecc44ffca88a23f55061cb
Harvonic <139082570+Harvonic@users.noreply.github.com>
2026-05-14 17:15:03 -0400

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int, int> seen;
+
+        for (int i=0; i < nums.size(); i++){
+            int remaining = target - nums[i];
+
+            if (seen.find(remaining) != seen.end()){
+                return {seen[remaining], i};
+            }
+
+            seen[nums[i]] = i;
+        }
+
+        return {};
+        
+    }
+};
