ebf5b9dfa34c4e85f3959e707a79046c0e320bf7
Harvonic <139082570+Harvonic@users.noreply.github.com>
2026-05-14 13:56:22 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if (s.size() != t.size()){
+            return false;
+        }
+        unordered_map<char, int> count;
+
+        for (int i=0; i < s.size(); i++){
+            count[s[i]]++;
+        }
+
+        for (int i=0; i < t.size(); i++){
+            if (count.count(t[i]) == 0){
+                return false;
+            }
+            count[t[i]]--;
+        }
+
+        for (int i=0; i < t.size(); i++){
+            if (count[t[i]] != 0){
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+};
